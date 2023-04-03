127d8a02cdb343afa8bbed245566152903c1cc80
Nurlan98J <126944044+Nurlan98J@users.noreply.github.com>
2023-04-03 22:45:46 +0400

    Update script1.js

--- script1.js
+++ script1.js
@@ -4,38 +4,38 @@ let task = document.querySelector("#task");
 let addTaskBtn = document.getElementById("addTask");
 let ul = document.querySelector("#task-list");
 
-// Получаем список задач из локального хранилища
+
 let tasks = JSON.parse(localStorage.getItem('tasks'));
 
-// Если ничего не было сохранено, создаем пустой массив
+
 if (!tasks) {
   tasks = [];
 }
 
-// Функция для сохранения списка задач в локальном хранилище
+
 function saveTasksToLocalStorage(tasks) {
   localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
-// Функция для добавления задачи в список
+
 function addTask(task) {
   tasks.push(task);
   saveTasksToLocalStorage(tasks);
 }
 
-// Функция для удаления задачи из списка
+
 function removeTask(index) {
   tasks.splice(index, 1);
   saveTasksToLocalStorage(tasks);
 }
 
-// Функция для обновления задачи в списке
+
 function updateTask(index, task) {
   tasks[index] = task;
   saveTasksToLocalStorage(tasks);
 }
 
-// При загрузке страницы выводим список задач
+
 function renderTasks() {
   ul.innerHTML = '';
 
@@ -60,7 +60,7 @@ function renderTasks() {
   });
 }
 
-// Обработчик события для добавления задачи в список
+
 addTaskBtn.addEventListener('click', (event) => {
   event.preventDefault();
   const taskValue = task.value.trim();
@@ -71,5 +71,5 @@ addTaskBtn.addEventListener('click', (event) => {
   }
 });
 
-// Выводим список задач при загрузке страницы
+
 renderTasks();
